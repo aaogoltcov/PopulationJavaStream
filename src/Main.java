c627e102a63ada2ea12e145f98a5596764e70c44
Aleksei Ogoltcov <aaogoltcov@mai.ru>
2023-06-12 13:14:30 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
+        List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
+        Collection<Person> persons = new ArrayList<>();
+
+        for (int i = 0; i < 10_000_000; i++) {
+            persons.add(new Person(
+                    names.get(new Random().nextInt(names.size())),
+                    families.get(new Random().nextInt(families.size())),
+                    new Random().nextInt(100),
+                    Sex.values()[new Random().nextInt(Sex.values().length)],
+                    Education.values()[new Random().nextInt(Education.values().length)])
+            );
+        }
+
+        // количество несовершеннолетних
+        long minorCount = persons
+                .stream()
+                .filter(person -> person.getAge() < 18)
+                .count();
+        System.out.println("Количество несовершеннолетних: " + minorCount);
+
+        // список фамилий призывников
+        List<String> conscripts = persons
+                .stream()
+                .filter(person -> {
+                    final int age = person.getAge();
+
+                    return age >= 18 && age < 27 && person.getSex() == Sex.MAN;
+                })
+                .map(Person::getFamily)
+//                .limit(10)
+                .toList();
+        System.out.println("Список призывников: " + conscripts);
+
+        // отсортированный по фамилии список потенциально работоспособных людей
+        // с высшим образованием
+        List<String> workForce = persons
+                .stream()
+                .filter(person -> {
+                    final Sex sex = person.getSex();
+                    final int age = person.getAge();
+                    final Education education = person.getEducation();
+
+                    return
+                            (sex == Sex.MAN && age >= 18 && age < 65 && education == Education.HIGHER) ||
+                                    (sex == Sex.WOMAN && age >= 18 && age < 60 && education == Education.HIGHER);
+                })
+                .map(Person::getFamily)
+                .sorted()
+//                .limit(10)
+                .toList();
+        System.out.println("Список рабочей силы с высшим образованием: " + workForce);
+    }
+}
